7c8617cf29f34d0f603007d1d0fd634567855f48
SnoopJeDi <snoopjedi@gmail.com>
2015-03-29 18:04:12 -0500

    photo extraction implemented

--- dbparser.py
+++ dbparser.py
@@ -2,37 +2,92 @@
 
 # Parses ormvmasterfile.xml
 
+from __future__ import print_function
+
+import os
+import sys
 import xml.etree.ElementTree as ET
 import json
+#import binascii
+from base64 import b64decode
+from collections import OrderedDict
 
 tree = ET.parse('./ormvmasterfile.xml')
 indb = tree.getroot()
 
-outdb = dict() 
+outdb = OrderedDict()
+outfile = file("ormvdb.json",'w')
 
-outdb[1] = "butt"
+imgdir = 'img'
 
+# Thanks, stackoverflow!
+def warning(*objs):
+    print("WARNING: ", *objs, file=sys.stderr)
+
+def getPhoto(rec):
+    rec.hasphoto = bool(rec.find("PHOTO").text)
+    if not rec.hasphoto :
+        print("INFO: no photo for record " + str(rec.recid))
+        return None
+
+    photo = rec.find("photo_x0020_attachments")
+
+    if photo is None :
+        print("INFO: Record " + str(rec.recid) + " has a photo listed, but no photo is present in the database.")
+        return None
+    else:
+        if not os.path.exists(imgdir):
+            os.makedirs(imgdir)
+        
+        #print("DEBUG: trying to decode "+str(photo.text))
+        try :
+            p = b64decode(photo.find("FileData").text.replace("\n","").replace("\r",""))
+            ext = '.' + p[12:18:2]
+            print("DEBUG: header is "+str(ext))
+            p = p[20:]
+            # must skip 20 bytes of MS header...
+            # TODO: consume these bytes and determine appropriate file extension
+            fn = str(rec.recid)+ext
+            f = file(imgdir+"/"+fn,'wb')
+            print(p,file=f)
+            f.close()
+            return fn
+        except :
+            warning("Could not decode photo for record " + str(rec.recid))
+            return None
+
+def getTag(rec,tag):
+    t  = rec.find(tag)
+    if t is None:
+        print("INFO: Tag " + tag + " for record " + str(rec.recid)+ "requested, but none found")
+    else :
+        return t.text
+
+def initRec(rec):
+    rec.recid = int(rec.find("REC").text)
+
+# XML is gross.
 for rec in indb:
 
-    recid = int(rec.find("REC").text)
-    fname = rec.find("FNAME").text
-    lname = rec.find("LNAME").text
+    initRec(rec)
 
-    att = rec.find("photo_x0020_attachments")
+    rec.fname = getTag(rec,"FNAME")
+    rec.lname = getTag(rec,"LNAME")
+    photo = getPhoto(rec)
+    rec.hometown = getTag(rec,"HOME")
+    rec.county = getTag(rec,"COUNTY")
 
-    '''
-    print "Record number " +\
-    rec.find("REC").text +\
-    ": " + rec.find("LNAME").text +\
-    ", " + rec.find("FNAME").text +\
-    " ~~ Has photo based on <PHOTO> tag: " + ("yes" if int(rec.find("PHOTO").text)>0 else "no")+\
-    " ~~ Has photo based on <FileData> tag: " + ("yes" if (att is not None and att.find("FileData") is not None) else "no")
-    '''
-
-    if recid in outdb:
-        print("WARNING: Duplicate record ID ({0}), skipping".format(recid))
+    if rec.recid in outdb:
+        warning("Duplicate record ID ({0}), skipping".format(rec.recid))
         continue
     else:
-        outdb[recid] = { 'fname':fname, 'lname':lname }
+        outdb[rec.recid] = OrderedDict([
+        ('fname',rec.fname)
+        ,('lname',rec.lname) 
+        ,('hasphoto',rec.hasphoto)
+        ,('photo',photo)
+        ,('hometown',rec.hometown)
+        ,('county',rec.county)
+        ])
     
-print json.dumps(outdb)
+print(json.dumps(outdb),file=outfile)
