c40b93b3fba00988b923e0d8476ba5c9ebb5d904
SnoopJeDi <snoopjedi@gmail.com>
2015-04-15 01:10:31 -0500

    skeleton of a GeoNames solution in place, has non-zero regex hits for hometown in all but 53/1046 cases

--- dbparser.py
+++ dbparser.py
@@ -11,14 +11,23 @@ import json
 #import binascii
 from base64 import b64decode
 from collections import OrderedDict
+import re
+#from geopy.geocoders import Nominatim
+#from geopy.exc import GeocoderTimedOut
+#geolocator = Nominatim()
 
 tree = ET.parse('./ormvmasterfile.xml')
 indb = tree.getroot()
 
 outdb = OrderedDict()
-outfile = file("ormvdb.json",'w')
+#outdb = []
+#outfile = file("ormvdb.json",'w')
+#print("[",file=outfile)
 
 imgdir = 'img'
+
+placesfile = file('placenames.txt','r')
+places = placesfile.readlines()
 
 # Thanks, stackoverflow!
 def warning(*objs):
@@ -32,43 +41,53 @@ def getPhoto(rec):
 
     photo = rec.find("photo_x0020_attachments")
 
-    if photo is None :
-        print("INFO: Record " + str(rec.recid) + " has a photo listed, but no photo is present in the database.")
-        return None
-    else:
-        if not os.path.exists(imgdir):
-            os.makedirs(imgdir)
-        
-        #print("DEBUG: trying to decode "+str(photo.text))
-        try :
-            p = b64decode(photo.find("FileData").text.replace("\n","").replace("\r",""))
-            ext = '.' + p[12:18:2]
-            print("DEBUG: header is "+str(ext))
-            p = p[20:]
-            # must skip 20 bytes of MS header...
-            # TODO: consume these bytes and determine appropriate file extension
-            fn = str(rec.recid)+ext
-            f = file(imgdir+"/"+fn,'wb')
-            print(p,file=f)
-            f.close()
-            return fn
-        except :
-            warning("Could not decode photo for record " + str(rec.recid))
+    doPhotos = False
+    if doPhotos :
+        if photo is None :
+            print("INFO: Record " + str(rec.recid) + " has a photo listed, but no photo is present in the database.")
             return None
+        else:
+            if not os.path.exists(imgdir):
+                os.makedirs(imgdir)
+            
+            #print("DEBUG: trying to decode "+str(photo.text))
+            try :
+                p = b64decode(photo.find("FileData").text.replace("\n","").replace("\r",""))
+                ext = '.' + p[12:18:2]
+                print("DEBUG: header is "+str(ext))
+                p = p[20:]
+                # must skip 20 bytes of MS header...
+                # TODO: consume these bytes and determine appropriate file extension
+                fn = str(rec.recid)+ext
+                f = file(imgdir+"/"+fn,'wb')
+                print(p,file=f)
+                f.close()
+                return fn
+            except :
+                warning("Could not decode photo for record " + str(rec.recid))
+                return None
 
 def getTag(rec,tag):
     t  = rec.find(tag)
     if t is None:
         print("INFO: Tag " + tag + " for record " + str(rec.recid)+ "requested, but none found")
+	return None
     else :
         return t.text
 
 def initRec(rec):
     rec.recid = int(rec.find("REC").text)
 
+def getLocation(name):
+    r = re.compile("^[^\t]+\t"+re.escape(name.lstrip()),re.IGNORECASE)
+    res = []
+    for p in places:
+	if r.search(p) is not None:
+		res.append(p)
+    return res
+
 # XML is gross.
 for rec in indb:
-
     initRec(rec)
 
     if rec.recid in outdb:
@@ -78,16 +97,44 @@ for rec in indb:
     rec.fname = getTag(rec,"FNAME")
     rec.lname = getTag(rec,"LNAME")
     photo = getPhoto(rec)
-    rec.hometown = getTag(rec,"HOME")
     rec.county = getTag(rec,"COUNTY")
+    
+    rec.hometown = getTag(rec,"HOME")
+    if rec.hometown is not None:
+        loc = getLocation(rec.hometown)
+        if len(loc) > 0:
+            print("Hometown (%s) successfully recovered! (recid %s)"% (rec.hometown,rec.recid))
+        else:
+            print("Hometown (%s) not recovered (recid %s)"% (rec.hometown,rec.recid))
+	
+    '''
+    try:
+	ht = geolocator.geocode(rec.hometown, timeout=30)
+    except:
+	print("Error: geocode failed")	
+    '''
+    ht = None
+    if ht is not None:
+	rec.latitude = ht.latitude
+    	rec.longitude = ht.longitude
+    else:
+	rec.latitude = -77
+	rec.longitude = 39
 
-    outdb[rec.recid] = OrderedDict([
+    outfile = file('json/'+str(rec.recid)+".json",'w')
+    outrec = OrderedDict( [
         ('fname',rec.fname)
         ,('lname',rec.lname) 
         ,('hasphoto',rec.hasphoto)
         ,('photo',photo)
         ,('hometown',rec.hometown)
-        ,('county',rec.county)
-        ])
+	,('latitude',rec.latitude)
+	,('longitude',rec.longitude)
+        ,('county',rec.county) ] )
+    outdb[rec.recid] = outrec
+    print(json.dumps(outrec), file=outfile)
+    #print(',', file=outfile)
+    outfile.close()
     
-print(json.dumps(outdb),file=outfile)
+#print(json.dumps(outdb),file=outfile)
+#print("]",file=outfile)
