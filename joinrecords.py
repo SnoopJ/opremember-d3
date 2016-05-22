c63758a8b93cc856057c096986a64fadb063b470
SnoopJeDi <snoopjedi@gmail.com>
2016-05-21 21:09:25 -0500

    fixed error in joinrecords output path

--- joinrecords.py
+++ joinrecords.py
@@ -3,21 +3,21 @@
 
 from __future__ import print_function
 
-import os,sys,glob
+import os, sys, glob
 import json
 from collections import OrderedDict
 import re
 
 recsbycounty = {}
-for f in glob.glob("json/*.json"):
+for f in glob.glob("json/[0-9]*.json"):
     print(f)
-    with open(f,'r') as infile:
+    with open(f, 'r') as infile:
         rec = json.loads(infile.read())
         recid = rec['recid']
         countyid = rec['countyid']
         if countyid not in recsbycounty.keys():
-            recsbycounty[countyid] = { 'countyid': countyid, 'casualties': [] }
+            recsbycounty[countyid] = {'countyid': countyid, 'casualties': []}
         recsbycounty[countyid]['casualties'].append(rec)
 
-with open('bycounty.json','w') as outfile:
-    json.dump( [ v for k,v in recsbycounty.items() ], outfile )
+with open('json/bycounty.json', 'w') as outfile:
+    json.dump([v for k, v in recsbycounty.items()], outfile)
